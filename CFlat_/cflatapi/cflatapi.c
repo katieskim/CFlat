f361ca10f667950e9a747a4b73a26e62f2646e17
Katie Kim <katherine.kim@columbia.edu>
2021-04-23 23:20:33 -0400

    cflatapi test

--- CFlat_/cflatapi/cflatapi.c
+++ CFlat_/cflatapi/cflatapi.c
@@ -9,8 +9,6 @@
 #include "midifile.h"
 #include "cflatapi.h"
 
-
-
 /*Mallocs space for and initializes a new note struct */
 struct note *new_note(char *tone, int octave, char *rhythm){
 
@@ -119,6 +117,7 @@ void add_note(struct note *note_ptr, MIDI_FILE *mf, int track){
     char tone = tlit[0];
     printf("%s\n", tlit);
     if (tone == 'R') {is_rest = 1;}
+    
     else if (tone == 'C') {miditone = 0;}
     else if (tone == 'D') {miditone = 2;}
     else if (tone == 'E') {miditone = 4;}
@@ -201,114 +200,4 @@ void play_tracks(int num_tracks, ...){
         
         midiFileClose(mf);
     }
-}
-
-/*DRIVER CODE FOR PLAY_NOTE_ARR: Creates a midifile with a C Major Scale */
-int main(int argc, char* argv[])
-{
-    /*Need the size of the array*/
-    
-    int num_notes = 12;
-    struct note *arr[num_notes + 1];
-
-    /* Creates space for the array */
-    int i;
-    for (i=0; i<num_notes; i++){
-        arr[i] = malloc(sizeof(struct note*));
-    }
-    
-    arr[num_notes] = NULL;
-    struct note *c = new_note("C", 4, "s");
-    struct note *d = new_note("D", 4, "s.");
-    struct note *e = new_note("E", 4, "e");
-    struct note *f = new_note("F+", 4, "e.");
-    struct note *g = new_note("G", 4, "q");
-    struct note *a = new_note("R", 4, "q.");
-    struct note *b = new_note("B", 4, "h");
-    struct note *c_2 = new_note("C", 5, "h.");
-    struct note *d_2 = new_note("R", 5, "w");
-    struct note *e_2 = new_note("E", 5, "w");
-    struct note *f_2 = new_note("F", 5, "w");
-    struct note *c_flat = new_note("C-", 5, "h");
-
-    arr[0] = c;
-    arr[1] = d;
-    arr[2] = e;
-    arr[3] = f;
-    arr[4] = g;
-    arr[5] = a;
-    arr[6] = b;
-    arr[7] = c_2;
-    arr[8] = d_2;
-    arr[9] = e_2;
-    arr[10] = f_2;
-    arr[11] = c_flat;
-/*
-    arr[9] = d_2;
-    arr[10] = d_s;
-    arr[11] = e_2;
-    arr[12] = f_2;
-    arr[13] = f_s;
-    arr[14] = g_2;
-    arr[15] = g_s;
-    arr[16] = a_2;
-    arr[17] = a_s;
-    arr[18] = b_2;
-    arr[20] = c_3;  */
-
-
-    bplay_note(c, 60);
-    iplay_note(c, 100);
-
-    play_note_arr(arr);
-    iplay_note_arr(arr, 17);
-
-
-    struct note *A[4];  A[3] = NULL;
-    struct note *B[4];  B[3] = NULL;
-    struct note *C[5];  C[4] = NULL;
-    struct note *D[5];  D[4] = NULL;
-
-    for (i=0; i<3; i++){
-        A[i] = malloc(sizeof(struct note*));
-        B[i] = malloc(sizeof(struct note*));
-        C[i] = malloc(sizeof(struct note*));
-    }
-    C[3] = malloc(sizeof(struct note*));
-    D[3] = malloc(sizeof(struct note*));
-     
-    struct note *et = new_note("E", 4, "h");
-    struct note *dt = new_note("D", 4, "h");
-    struct note *ct = new_note("C", 4, "h");
-
-    struct note *gt = new_note("G", 4, "h");
-    struct note *ft = new_note("F", 4, "h.");
-
-    struct note *ctt = new_note("C", 5, "h");
-    struct note *dtt = new_note("D", 5, "w.");
-    struct note *ett = new_note("E", 5, "h");
-    struct note *at = new_note("A", 4, "h");
-
-    
-
-    A[0] = et;
-    A[1] = dt;
-    A[2] = ct;
-
-    B[0] = gt;
-    B[1] = ft;
-    B[2] = et;
-
-    C[0] = ctt;
-    C[1] = at;
-    C[2] = gt;
-    C[3] = ctt;
-
-    D[0] = ctt;
-    D[1] = dtt;
-    D[2] = ctt;
-    D[3] = ett;
-
-    play_tracks(4, A, B, C, D);
-	return 0;
 }
--- CFlat_/cflatapi/cflatapi.h
+++ CFlat_/cflatapi/cflatapi.h
@@ -7,17 +7,26 @@ struct note{
     int olit;
     char rlit[3];
 }note;
+/*Allocates space for a note struct*/
+struct note *new_note(char *tone, int octave, char *rhythm);
 
-/*Function declarations*/
+/*
+** CFLAT USER FUNCTION DECLARATIONS
+*/
 
-/*INPUT: Takes in a pointer to a single note struct */ 
-/*OUTPUT: A midifile called "hellonote.mid" that plays the note */ 
+/* Plays a single note
+**INPUT: Takes in a pointer to a single note struct 
+**OUTPUT: A midifile called "(i/b)hellonote.mid" that plays the note 
+*/
 void play_note(struct note *n); 
 void bplay_note(struct note *n, int beat); /* bplay_note takes in beat (beats/min) */
 void iplay_note(struct note *n, int instrument);  /* iplay_note takes in an instrument 1-127*/
 
-/*INPUT: Takes in a pointer to an array of note struct pointers */ 
-/*OUTPUT: A midifile called "notearray.mid" that plays a C Major scale.  */ 
+
+/* Plays a single note
+**INPUT: Takes in a pointer to an array of note struct pointers
+**OUTPUT: A midifile called "notearray.mid" that plays a C Major scale.
+*/
 void play_note_arr(struct note *note_arr[]);
 void bplay_note_arr(struct note *note_arr[], int beat); /* bplay_note_arr takes in beat (beats/min) */
 void iplay_note_arr(struct note *note_arr[], int instrument);
